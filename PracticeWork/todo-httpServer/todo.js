f1e1a3913a9304273ac53362497e24389176bf63
Dixita-mishra <dixitamishra23@gmail.com>
2026-03-22 23:36:13 +0530

    commit for modifications

--- PracticeWork/todo-httpServer/todo.js
+++ PracticeWork/todo-httpServer/todo.js
@@ -7,7 +7,7 @@ const server=http.createServer((req,res)=>{
     const url=new URL(req.url,`http://${req.headers.host}`);
     const pathname=url.pathname;
     if(method==="GET" && pathname==="/todos"){
-        fs.readFile(todo,"utf-8",(err,data)=>{
+        fs.readFile(path,"utf-8",(err,data)=>{
             if(err){
                 res.writeHead(500);
                 res.end("Error in fetching data");
@@ -16,6 +16,36 @@ const server=http.createServer((req,res)=>{
             res.writeHead(200,{"Content-Type":"application/json"});
             res.end(data);
 
+        })
+    }
+    else if(method==="POST" && pathname==="/todos"){
+        let body="";
+        req.on("data",(chunk)=>{
+            body+=chunk;
+        });
+        req.on("end",()=>{
+            const parsed=JSON.parse(body);
+            fs.readFile(path,"utf-8",(err,data)=>{
+                if(err){
+                    res.writeHead(500);
+                    res.end("error in fetching data");
+                    return;
+                }
+                const note=JSON.parse(data);
+                const id=note.length?note[note.length-1].id+1:1;
+                const newtodo={id:id,title:parsed.title,completed:false};
+                note.push(newtodo);
+                fs.writeFile(path,JSON.stringify(note),(err)=>{
+                    if(err){
+                        res.end("error in writing data");
+                        return;
+                    }
+                    res.writeHead(201);
+                    res.end("notes successfully written");
+                })
+                
+
+            })
         })
     }
 })
